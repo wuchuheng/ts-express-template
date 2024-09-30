c52da7e60b37968712e9995cc800dcbd2beff9bf
wuchuheng <wuchuheng@wuchuhengs-MacBook-Air.local>
2024-10-01 04:02:55 +0800

    fix: make the printing message more frendly when the installed cli successful#2

--- bin/cli.js
+++ bin/cli.js
@@ -24,12 +24,12 @@ async function main() {
     execSync(`cd ${projectDir} && pnpm install && pnpm run build`);
 
     console.log("The installation is done!");
-    console.log(`
-Done. Now run:
-cd vue-tmp
+    console.log(` Done. Now run:`);
+
+    console.log(`\x1b[1m\x1b[32m
+cd ${projectName}
 pnpm install
-pnpm format
-pnpm dev
+pnpm dev \x1b[0m
 `);
   } catch (error) {
     console.error("An error occurred:", error.message);
